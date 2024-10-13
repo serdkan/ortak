4630a7a1f1a8a617c6617213230e705d4de46b7d
Wq1fpsz <efegl3535@gmail.com>
2024-10-13 13:56:20 +0300

    ödev

--- efe.js
+++ efe.js
@@ -3,8 +3,8 @@ const data = await fetch("https://jsonplaceholder.org/users").then((response) =>
 );
 
 for (let veri of data) {
-  let ilkkarakter = veri.address.zipcode.substring(0, 1);
-  if (ilkkarakter == 1) {
-    console.log(veri.address.zipcode);
+  let ilkkarakter = veri.company.name.substring(0, 1);
+  if (ilkkarakter == "G") {
+    console.log(veri.company.name);
   }
 }
