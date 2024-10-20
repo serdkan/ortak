2880cdd1d509c5e4ee6292cb758c6b6b33d40e01
Wq1fpsz <efegl3535@gmail.com>
2024-10-20 19:58:22 +0300

    19Ekimödevefe

--- efe.js
+++ efe.js
@@ -1,14 +1,57 @@
-async function Odev(isim, soyisim) {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
-    response.json()
-  );
+let students = [
+  {
+    name: "Efe",
+    dersler: {
+      ders1: "Web Programcılığı"
+    },
+    notlar: {
+      sinav1: 100,
+      sinav2: 88
+    }
+  },
+  {
+    name: "Sena",
+    dersler: {
+      ders1: "Matematik",
 
-  for (let veri of data) {
-    const [first, last] = veri.name.split(" ");
-    if (first === isim && last === soyisim) {
-      return true;
+    },
+    notlar: {
+      sinav1: 100,
+      sinav2: 90
+    }
+  },
+  {
+    name: "Ela",
+    dersler: {
+      ders1: "İngilizce"
+    },
+    notlar: {
+      sinav1: 80,
+      sinav2: 90
+    }
+  },
+  {
+    name: "Buse",
+    dersler: {
+      ders1: "Edebiyat"
+    },
+    notlar: {
+      sinav1: 70,
+      sinav2: 65
+    }
+  },
+  {
+    name: "Zeynep",
+    dersler: {
+      ders1: "Türkçe"
+    },
+    notlar: {
+      sinav1: 90,
+      sinav2: 85
     }
   }
-  return false;
-}
-Odev("John", "Doe").then(result => console.log(result));
+];
+students.forEach(student => {
+  let ortalama = (student.notlar.sinav1 + student.notlar.sinav2) / 2;
+  console.log(`İsim: ${student.name} Dersi: ${student.dersler.ders1} Not Ortalaması: ${ortalama}`);
+});
