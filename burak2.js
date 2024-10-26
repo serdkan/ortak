9af548211172ad5e4205fdacc0f10ae317182ab4
serdkan <serdar.kandiran@outlook.com>
2024-10-26 21:39:51 +0300

    cardlar

--- burak2.js
+++
@@ -1,9 +0,0 @@
-async function kontrol() {
-  const gelenArray = await fetch("https://fakestoreapi.com/products").then((response) =>
-    response.json()
-  );
-
-  console.log(gelenArray);
-
-}
-kontrol();
