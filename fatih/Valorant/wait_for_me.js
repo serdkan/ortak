70fefb4ecbbaa6642295a275cef20d929925f190
LimonSTAR5555 <biguktibi@gmail.com>
2024-11-11 21:45:15 +0300

    kurtt

---
+++ fatih/Valorant/wait_for_me.js
@@ -0,0 +1,6 @@
+function anasayfaresim(){
+
+    document.getElementById("resim1").innerHtml = `<https://tasbihdigital.com/wp-content/uploads/2022/10/Valorant-Wallpaper-For-Desktop.jpg>`;
+    document.getElementById("resim2").innerHtml = `<https://c4.wallpaperflare.com/wallpaper/301/237/68/valorant-video-game-art-hd-wallpaper-preview.jpg>`;
+    document.getElementById("resim3").innerHtml = `<https://tasbihdigital.com/wp-content/uploads/2022/10/HD-Valorant-Wallpaper.jpg>`;
+}
