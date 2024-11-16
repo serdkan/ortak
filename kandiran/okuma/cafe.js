f093ee8492418cf253b3ed1ff0d1eeac703c11c2
serdkan <serdar.kandiran@outlook.com>
2024-11-16 22:39:52 +0300

    okuma

--- kandiran/okuma/cafe.js
+++ kandiran/okuma/cafe.js
@@ -1,23 +1,10 @@
-const text = `Herkes tarafından bilinen gerçekleri, planlanan eylemleri anlatırken, belli bir süre zarfında tekrarlanan eylemleri, değişmeyen durum, duygu ve arzuları ifade ederken kullanılır.
-Simple present tense alışkanlıkları, rutinleri veya sürekli tekrarlanan eylemler anlatırken kullanılır.
-I drink two cups of coffee every morning.
-He goes to the theater once a week.
-Değişmeyen durum, duygu ve arzuları ifade ederken kullanılır.
-She works in London
-I like chocolate
-Planlanan eylemleri anlatırken kullanılır.
-He takes an exam in April
-My family come to us on Monday
-Herkes tarafından bilinen gerçekleri anlatırken kullanılır.
-The sun sets in the west
-Water boils in 100 C
-Talimat ve yön bildirirken ingilizce geniş zaman kullanılır.
-The contents into hot water.
-You take the No:12 bus to Kadıköy
-Önceden programlanmış olayları anlatırken kullanılır. Bu kullanım türünde cümleler her ne kadar gelecek zaman anlamı taşısa bile geniş zaman kurallarına göre ifade edilir.
-The exam starts at 10 o’clock tomorrow.
-The ship leaves Kabatas at 10pm tonight
-Özellikle çocuk kitaplarında hikaye anlatımında kullanılır.`;
+const text = `Amentü şöyledir:
+Âmentü billahi ve melaiketihi ve kütübihi ve rüsülihi vel yevmil ahiri ve bilkaderi hayrihi ve şerrihi minallahi teâlâ vel ba'sü ba'del mevti hakkun. Eşhedü en lâ ilahe illallah ve eşhedü enne Muhammeden abdühü ve resülühü.
+[Yani, Allah’a, meleklerine, gönderdiği kitaplarına, peygamberlerine, ahiret gününe, kadere, hayrın ve şerrin Allah’tan olduğuna, öldükten sonra dirilmeye inanıyorum. Allah’tan başka ilah olmadığına ve Muhammed aleyhisselamın da Allah’ın kulu ve son Peygamberi olduğuna şehadet ediyorum.]
+
+İman, Muhammed aleyhisselamın, Peygamber olarak bildirdiği dini, akla, tecrübeye ve felsefeye uygun olup olmadığına bakmadan tasdik etmek yani kabul edip, beğenip, inanmaktır. Akla uygun olduğu için tasdik etmek, aklı tasdik etmek olur, Resulü tasdik etmek olmaz. Yahut Resulü ve aklı birlikte tasdik etmek olur ki, o zaman Peygambere itimat tam olmaz. Tam olmayınca, iman olmaz. Allahü teâlâ, (Onlar gayba [görmedikleri halde Resulümün bildirdiği her şeye] iman ederler) buyuruyor. (Bekara 3) Resulü de, (Dini [hükümleri, dinde bildirilenleri] aklı ile ölçenden daha zararlısı yoktur) buyurdu. (Taberani)
+
+Nazara yani göz değmesine inanmayan bir kimse, (Bugün fen, gözle görülemeyen şuaların iş yaptığını açıklıyor. Mesela bir kumanda ile TV’yi, radyoyu veya arabamızı açıp kapatabiliyoruz. Bunun için gözlerden çıkan şuanın zarar verebileceğine artık inanıyorum) dese bunun kıymeti olmaz. Çünkü bu insan dine değil, kumandadan çıkan şuaya inanıyor. Yahut şua ile birlikte Peygambere inanıyor. Yani fen kabul ettiği için, şuaların etkisini gözü ile gördüğü için inanıyor ki bu iman olmaz. Dinde bildirilen her şeyi, fen ispat edemese de, fayda veya zararını gözü ile görmese de, yine inanmak lazımdır. Hakiki iman gayba inanmaktır yani görmeden inanmaktır. Gördükten sonra artık o iman olmaz. Gördüğünü itiraf etmek olur. Bekara suresinin 3. âyetinde, gayba inanmak, görmeden inanmak övülüyor. İmanın altı şartı da gayba inanmayı gerektirmektedir. Çünkü hiç birini görmüş değiliz.`;
 
 const yazisisi = text.split(" ");
 
