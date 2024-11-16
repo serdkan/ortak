8b2563670acb6ef52b6a648c040e05249acabd16
serdkan <serdar.kandiran@outlook.com>
2024-11-16 22:10:53 +0300

    okuma

---
+++ serdar/menu.js
@@ -0,0 +1,12 @@
+document.getElementById(
+  "menu"
+).innerHTML = `
+<a class="btn btn-danger" href="/ahmet">Ahmet Aslan</a>
+<a class="btn btn-info" href="/ahmetaldemir/">Ahmet Aldemir</a>
+<a class="btn btn-info" href="/ba">Ba</a>
+<a class="btn btn-info" href="/burak">Burak</a>
+<a class="btn btn-info" href="/efe">Efe</a>
+<a class="btn btn-info" href="/fatih">Fatih</a>
+<a class="btn btn-info" href="/mazhar">Mazhar</a>
+<a class="btn btn-info" href="/Mustafa">Mustafa</a>
+<a class="btn btn-info" href="/omer" target="_blank">Ömer A</a>`;
